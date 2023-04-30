13498cf82bb3d7f7e1a5b2611d36065d88781340
janeesh27 <janeesh.tin@gmail.com>
2023-05-01 00:25:24 +0530

    added new component

--- src/app/components/Hero/index.jsx
+++ src/app/components/Hero/index.jsx
@@ -4,19 +4,33 @@ import styles from './index.module.scss';
 
 const Hero = () => {
     return (
-        <main className={styles.main}>
-            <Title heading="iPhone 14 Pro" subHeading="Pro. Beyond" />
+        <>
+            <main className={styles.main}>
+                <Title heading="iPhone 14 Pro" subHeading="Pro. Beyond" />
 
-            <div className={styles.linkContainer}>
-                <a href="#">Learn more </a>
-            </div>
+                <div className={styles.linkContainer}>
+                    <a href="#">Learn more </a>
+                </div>
 
-            <div className={styles.videoContainer}>
-                <video loop muted playsInline autoPlay>
-                    <source src="media/heroVideo.mp4" />
-                </video>
-            </div>
-        </main>
+                <div className={styles.videoContainer}>
+                    <video loop muted playsInline autoPlay>
+                        <source src="media/heroVideo.mp4" />
+                    </video>
+                </div>
+            </main>
+
+            <section className={styles.section}>
+                <Title heading="iPhone 14" subHeading="Wonderfull." black />
+
+                <div className={styles.linkContainer}>
+                    <a href="#">Learn more </a>
+                </div>
+
+                <div className={styles.imgContainer}>
+                    <img src="/media/iphoneImages.png" alt="" />
+                </div>
+            </section>
+        </>
     );
 };
 
--- src/app/components/Title/index.jsx
+++ src/app/components/Title/index.jsx
@@ -1,8 +1,8 @@
 import styles from './index.module.scss';
 
-const Title = ({ heading, subHeading }) => {
+const Title = ({ heading, subHeading, black }) => {
     return (
-        <div className={styles.headingContainer}>
+        <div className={`${styles.headingContainer} ${black && styles.black}`}>
             <h1>{heading}</h1>
             <h2>{subHeading}</h2>
         </div>
