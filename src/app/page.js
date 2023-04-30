916afbd23dd5b1a0a69450f82235288373609ffc
janeesh <janeesh.singh@ecomexperts.io>
2023-05-01 04:46:47 +0530

    footer section improvement

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
             <AppleWatch />
             <ProductItems />
             <Exchange />
-            {/* <Footer /> */}
+           <Footer /> 
         </>
     );
 }
