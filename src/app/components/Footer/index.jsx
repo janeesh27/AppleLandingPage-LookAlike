0a2cb52d6264a7c5fefedae0f09cc006dc6ca05e
janeesh27 <janeesh.tin@gmail.com>
2023-05-01 21:01:49 +0530

    fix for footer

--- src/app/components/Footer/index.jsx
+++ src/app/components/Footer/index.jsx
@@ -1,19 +1,19 @@
 import styles from './index.module.scss';
 
 const index = () => {
-  return (
-    <div className={styles.footer}>
-      <div className={styles.footerlinks}>
-      <div className={styles.linebreak}>
-       <p> Copyright © 2023 Apple Inc. All rights reserved.</p>
-        <a href="#">Privacy Policy</a> 
-        <a href="#">Terms of Use</a> 
-        <a href="#">Sales and Refunds</a>  
-        <a href="#">Legal Site Map</a>
-      </div>
-      </div>
-  </div>
-  )
-}
+    return (
+        <div className={styles.footer}>
+            <div>
+                <p> Copyright © 2023 Apple Inc. All rights reserved.</p>
+            </div>
+            <div className={styles.footerlinks}>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Terms of Use</a>
+                <a href="#">Sales and Refunds</a>
+                <a href="#">Legal Site Map</a>
+            </div>
+        </div>
+    );
+};
 
-export default index
+export default index;
