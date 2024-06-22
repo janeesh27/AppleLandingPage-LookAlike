5df6c767f60274179147915ba095fa13e44e760d
RvtjSngh <rvtjsngh@gmail.com>
2024-06-22 21:39:15 +0530

    Code Push

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,24 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Hero from './components/Hero';
-import AppleWatch from './components/AppleWatch';
-import ProductItems from './components/ProductItems';
-import Exchange from './components/Exchange';
-import Airpods from './components/Airpods';
-import ArGlasses from './components/Arglasses';
-import Slider from './components/Slider';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import AppleWatch from "./components/AppleWatch";
+import ProductItems from "./components/ProductItems";
+import Exchange from "./components/Exchange";
+import Airpods from "./components/Airpods";
+import ArGlasses from "./components/Arglasses";
+import Slider from "./components/Slider";
 
 export default function Home() {
-    return (
-        <>
-            <Header />
-            <Hero />
-            <AppleWatch />
-            {/* <ProductItems /> */}
-            <Airpods />
-            <ArGlasses />
-            <Exchange />
-            <Slider />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Hero />
+      <AppleWatch />
+      <Airpods />
+      <ArGlasses />
+      <Exchange />
+      <Slider />
+      <Footer />
+    </>
+  );
 }
